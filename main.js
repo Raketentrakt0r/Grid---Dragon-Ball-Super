606172e8bcc80fc86f3b32ff998c7f1f1eff1d45
Trakt0r <raketentrakt0r@gmail.com>
2021-01-18 10:51:15 +0100

    First commit

---
+++ main.js
@@ -0,0 +1,27 @@
+const modal = document.querySelector(".modal");
+const previews = document.querySelectorAll(".container img");
+const original = document.querySelector(".full-img"); 
+const text = document.querySelector(".text");
+
+previews.forEach(preview => {
+    preview.addEventListener("click", ()=> {
+        modal.classList.add("open");
+        original.classList.add("open");
+
+        // Dynamic change text and image
+        const originalSrc = preview.getAttribute("data-original");
+        original.src = `./img/${originalSrc}`;
+        /*original.src = originalSrc; Esto es lo mismo*/
+        const altText = preview.alt;
+        text.textContent = altText;
+    });
+
+});
+
+modal.addEventListener("click", (e) => {
+    if(e.target.classList.contains("modal")){
+        modal.classList.remove("open");
+        original.classList.remove("open");
+    }
+
+});
